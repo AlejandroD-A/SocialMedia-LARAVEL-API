a35de25dc383f81ca6fc271bde35490f4111b5a0
AleDA <alejandrodaliaga@gmail.com>
2021-01-27 19:07:05 -0300

    fix a arrow error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if (env('REDIRECT_HTTPS')) {
-            $this->app['request']->server - set('HTTPS', true);
+            $this->app['request']->server->set('HTTPS', true);
         }
     }
 
