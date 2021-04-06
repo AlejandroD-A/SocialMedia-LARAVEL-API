632986cecf0de356296f9ed07c08fc781a805e72
AleDA <alejandrodaliaga@gmail.com>
2021-04-06 19:52:34 -0300

    Se mantuvo avatar dentro de user para facilitar requests

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar')->default('none');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
